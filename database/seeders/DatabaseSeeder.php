ccfcc5e00376ebe7e2e6d0b275471369b5e8d8cc
Abel <ridhosakti7@gmail.com>
2025-10-30 08:50:46 +0700

    feat: seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,7 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use App\Models\User;
 
-class UserSeeder extends Seeder
+class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
